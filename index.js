1a2546cc324b683b0347126e117cefa542d3303c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-05 14:24:00 +0300

    creates a js file for DOM nodes manipulation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+// Selects the <main> element and removes it
+document.querySelector("main");
+main.remove();
+
+//creates a new <h1> node
+const newHeader = document.createElement("h1");
+
+//This will append the <h1> to body
+//document.body.append(newHeader);
+
+//sets the id of <h1> to 'victory'
+//newHeader.setAttribute('id', 'victory');
+newHeader.id = "victory"
+
+//sets the text content of <h1>
+newHeader.textContent = "Brendah Kiragu is the champion"
+
+
+
